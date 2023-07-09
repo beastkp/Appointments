cbdc9fa14bb62b500024c5144959558fbd6b0fda
beastkp <kroshpan@gmail.com>
2023-07-09 23:12:38 +0530

    Protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,82 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
-import Contact from './pages/Contact'
-import Work from './pages/Service'
+import Contact from "./pages/Contact";
+import Work from "./pages/Service";
 import Appointment from "./pages/Appointment";
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+
 function App() {
+  const { loading } = useSelector((store) => store.alerts);
+
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route  path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />        
-        <Route path="/services" element={<Work/>} />        
-        <Route path="/appointment" element={<Appointment/>} />        
-
-      </Routes>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <ProtectedRoute>
+                <About />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <ProtectedRoute>
+                <Contact />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/services"
+            element={
+              <ProtectedRoute>
+                <Work />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/appointment"
+            element={
+              <ProtectedRoute>
+                <Appointment />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      )}
     </Router>
   );
 }
